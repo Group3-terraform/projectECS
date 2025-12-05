7c293e404d521cd5819ea663f5d5bb0717ec37f3
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-05 14:55:13 +0700

    init update

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,21 +1,29 @@
-########################################
-# Load modules
-########################################
-
-module "alb" {
-  source       = "../../modules/alb"
-  project_name = var.project_name
-  environment  = var.environment
-
-  vpc_id         = module.vpc.vpc_id
-  public_subnets = module.vpc.public_subnets
-
-  security_groups = [module.security.alb_sg_id]
-
-  acm_arn = module.acm.acm_arn
+terraform {
+  backend "s3" {
+    bucket         = "group3-tfstate-dev"
+    key            = "ecs/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "group3-tfstate-lock"
+  }
 }
 
+provider "aws" {
+  region = var.region
+}
 
+# ----------------------
+# VPC
+# ----------------------
+module "vpc" {
+  source        = "../../modules/vpc"
+  project_name  = var.project_name
+  environment   = var.environment
+  region        = var.region
+}
+
+# ----------------------
+# SECURITY
+# ----------------------
 module "security" {
   source       = "../../modules/security"
   vpc_id       = module.vpc.vpc_id
@@ -23,103 +31,74 @@ module "security" {
   environment  = var.environment
 }
 
-
-module "iam" {
-  source       = "../../modules/iam"
-  project_name = var.project_name
-  environment  = var.environment
-}
-
+# ----------------------
+# ACM Certificate
+# ----------------------
 module "acm" {
   source       = "../../modules/acm"
   project_name = var.project_name
   environment  = var.environment
-
-  # Single domain for DEV
-  domain       = "api.dev.theareak.click"
-
-  # Your Route53 Hosted Zone ID
-  zone_id      = "Z07852252OWMU8O090PPL"
-}
-
-
-# module "alb" {
-#   source       = "../../modules/alb"
-#   project_name = var.project_name
-#   environment  = var.environment
-
-#   vpc_id         = module.vpc.vpc_id
-#   public_subnets = module.vpc.public_subnets
-
-#   security_groups = [module.security.alb_sg_id]
-
-#   acm_arn = module.acm.acm_arn
-# }
-
-
-module "route53" {
-  source       = "../../modules/route53"
-  project_name = var.project_name
-  environment  = var.environment
-
-  domain       = var.domain_name
-
-  alb_dns_name = module.alb.alb_dns_name
-  alb_zone_id  = module.alb.alb_zone_id
-
+  domain       = var.domain
   zone_id      = var.zone_id
 }
 
+# ----------------------
+# ALB
+# ----------------------
+module "alb" {
+  source          = "../../modules/alb"
+  project_name    = var.project_name
+  environment     = var.environment
+  vpc_id          = module.vpc.vpc_id
+  public_subnets  = module.vpc.public_subnets
+  security_groups = [module.security.alb_sg_id]
+  acm_arn         = module.acm.certificate_arn
+}
 
+# ----------------------
+# IAM
+# ----------------------
+module "iam" {
+  source        = "../../modules/iam"
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+# ----------------------
+# ECS
+# ----------------------
 module "ecs" {
-  source               = "../../modules/ecs"
-  project              = var.project
-  environment          = var.environment
-  region               = var.region
+  source              = "../../modules/ecs"
+  project_name        = var.project_name
+  environment         = var.environment
+  vpc_id              = module.vpc.vpc_id
+  private_subnets     = module.vpc.private_subnets
+  alb_listener_arn    = module.alb.https_listener_arn
+  task_execution_role = module.iam.ecs_task_execution_role_arn
+  task_role           = module.iam.ecs_task_role_arn
 
   service_a_image = var.service_a_image
   service_b_image = var.service_b_image
   service_c_image = var.service_c_image
-
-  private_subnets = module.vpc.private_subnets
-  security_groups = [module.security.ecs_sg_id]
-
-  tg_service_a = module.alb.tg_service_a
-  tg_service_b = module.alb.tg_service_b
-  tg_service_c = module.alb.tg_service_c
-
-  alb_listener = module.alb.https_listener_arn
-
-  ecs_task_role           = module.iam.ecs_task_role
-  ecs_task_execution_role = module.iam.ecs_task_execution_role
-
-  common_env = [
-    { name = "ENV",        value = var.environment },
-    { name = "PROJECT",    value = var.project },
-    { name = "REGION",     value = var.region }
-  ]
 }
 
-########################################
+# ----------------------
+# Route 53
+# ----------------------
+module "route53" {
+  source       = "../../modules/route53"
+  project_name = var.project_name
+  environment  = var.environment
+  domain       = var.domain
+  zone_id      = var.zone_id
+  alb_dns_name = module.alb.dns_name
+  alb_zone_id  = module.alb.zone_id
+}
+
+# ----------------------
 # OUTPUTS
-########################################
-
-output "alb_url" {
-  value = module.alb.alb_dns
-}
-
-output "api_base" {
-  value = "https://${var.domain_name}"
-}
-
-output "service_a_url" {
-  value = "https://${var.domain_name}/a"
-}
-
-output "service_b_url" {
-  value = "https://${var.domain_name}/b"
-}
-
-output "service_c_url" {
-  value = "https://${var.domain_name}/c"
-}
+# ----------------------
+output "alb_url"     { value = "https://${var.domain}" }
+output "service_a"   { value = "https://${var.domain}/a" }
+output "service_b"   { value = "https://${var.domain}/b" }
+output "service_c"   { value = "https://${var.domain}/c" }
--- envs/dev/terraform.tfvars
+++ envs/dev/terraform.tfvars
@@ -2,8 +2,8 @@ project_name = "group3"
 environment  = "dev"
 region       = "ap-southeast-1"
 
-domain       = "api.dev.theareak.click"
-zone_id      = "Z07852252OWMU8O090PPL"
+domain  = "api.dev.theareak.click"
+zone_id = "Z07852252OWMU8O090PPL"
 
 service_a_image = "570430250751.dkr.ecr.ap-southeast-1.amazonaws.com/service-a:dev-v1.0.7"
 service_b_image = "570430250751.dkr.ecr.ap-southeast-1.amazonaws.com/service-b:dev-v1.0.7"
