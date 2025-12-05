cc9b21abcfd70e12ce25873683d7f241bd0827db
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-05 14:28:43 +0700

    init update

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -29,7 +29,3 @@ resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
   validation_record_fqdns = [for r in aws_route53_record.validation : r.fqdn]
 }
-
-output "certificate_arn" {
-  value = aws_acm_certificate.this.arn
-}
