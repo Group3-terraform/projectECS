c5e1244239b29c682fdc5705669f843648187131
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-05 14:35:17 +0700

    init update

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -22,7 +22,3 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
-output "ecs_task_execution_role_arn" {
-  value = aws_iam_role.ecs_task_execution.arn
-}
